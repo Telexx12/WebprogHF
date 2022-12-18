23cd8c947b2e4edb4fec4e8b919fcf868e27f2d1
Telexx12 <telegdi.david12@gmail.com>
2022-12-18 10:30:13 +0200

    negyedik

---
+++ HF4/Loader.php
@@ -0,0 +1,7 @@
+<?php
+    spl_autoload_register(function ($classname){
+        $file = $classname . ".php";
+        if(file_exists($file)){
+            include $file;
+        }
+    });
---
+++ HF4/index.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once "Loader.php";
+
+$product1 = new Product(1, "iPhone 11", 2500, 10);
+$product2 = new Product(2, "M2 SSD", 400, 10);
+$product3 = new Product(3, "Samsung Galaxy S20", 3200, 10);
+$cart = new Cart();
+$cartItem1 = $cart->addProduct($product1, 1);
+$cartItem2 = $product2->addToCart($cart, 1);
+echo "Number of items in cart: ".PHP_EOL;
+echo $cart->getTotalQuantity().PHP_EOL; // This must print 2
+echo "<br>";
+echo "Total price of items in cart: ".PHP_EOL;
+echo $cart->getTotalSum().PHP_EOL; // This must print 2900
+echo "<br>";
+
+
+$cartItem2->increaseQuantity();
+$cartItem2->increaseQuantity();
+
+
+
+echo "Number of items in cart: ".PHP_EOL;
+echo $cart->getTotalQuantity().PHP_EOL; // This must print 4
+echo "<br>";
+
+echo "Total price of items in cart: ".PHP_EOL;
+echo $cart->getTotalSum().PHP_EOL; // This must print 3700
+echo "<br>";
+
+
+$cart->removeProduct($product3);
+
+echo "Number of items in cart: ".PHP_EOL;
+echo $cart->getTotalQuantity().PHP_EOL; // This must print 4
+echo "<br>";
+
+
+echo "Total price of items in cart: ".PHP_EOL;
+echo $cart->getTotalSum().PHP_EOL; // This must print 3700
